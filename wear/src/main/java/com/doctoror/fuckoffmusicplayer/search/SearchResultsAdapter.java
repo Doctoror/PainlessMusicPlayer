8889f34260a92ac5e8a2a5db77207292f1f72d15
Yaroslav Mytkalyk <docd1990@gmail.com>
2016-11-23 15:34:55 +0200

    Changed search results appearance.

--- wear/src/main/java/com/doctoror/fuckoffmusicplayer/search/SearchResultsAdapter.java
+++ wear/src/main/java/com/doctoror/fuckoffmusicplayer/search/SearchResultsAdapter.java
@@ -5,10 +5,11 @@ import com.doctoror.fuckoffmusicplayer.R;
 import com.doctoror.fuckoffmusicplayer.base.BaseRecyclerAdapter;
 
 import android.content.Context;
-import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
@@ -20,20 +21,24 @@ import java.util.List;
 final class SearchResultsAdapter
         extends BaseRecyclerAdapter<SearchResultsAdapterItem, SearchResultsViewHolder> {
 
-    private static final int VIEW_TYPE_HEADER = 0;
-    private static final int VIEW_TYPE_ALBUM = 1;
-    private static final int VIEW_TYPE_ARTIST = 2;
-    private static final int VIEW_TYPE_TRACK = 3;
+    private static final int VIEW_TYPE_ALBUM = 0;
+    private static final int VIEW_TYPE_ARTIST = 1;
+    private static final int VIEW_TYPE_TRACK = 2;
 
-    @NonNull
-    private final Resources mRes;
+    private final Drawable mIconAlbum;
+    private final Drawable mIconArtist;
+    private final Drawable mIconTrack;
 
     private WearSearchData.Results mResults;
 
     SearchResultsAdapter(@NonNull final Context context,
             @Nullable final WearSearchData.Results results) {
         super(context);
-        mRes = context.getResources();
+
+        mIconAlbum = context.getDrawable(R.drawable.list_item_icon_album_40dp);
+        mIconArtist = context.getDrawable(R.drawable.list_item_icon_artist_40dp);
+        mIconTrack = context.getDrawable(R.drawable.list_item_icon_track_40dp);
+
         setResults(results);
     }
 
@@ -50,7 +55,6 @@ final class SearchResultsAdapter
         final List<SearchResultsAdapterItem> items = getMutableItems();
         items.clear();
         if (results != null) {
-            items.add(new SearchResultsAdapterItemHeader(mRes.getText(R.string.Albums)));
             final WearSearchData.Album[] albums = results.albums;
             if (albums != null) {
                 for (int i = 0; i < albums.length; i++) {
@@ -58,7 +62,6 @@ final class SearchResultsAdapter
                 }
             }
 
-            items.add(new SearchResultsAdapterItemHeader(mRes.getText(R.string.Artists)));
             final WearSearchData.Artist[] artists = results.artists;
             if (artists != null) {
                 for (int i = 0; i < artists.length; i++) {
@@ -66,7 +69,6 @@ final class SearchResultsAdapter
                 }
             }
 
-            items.add(new SearchResultsAdapterItemHeader(mRes.getText(R.string.Tracks)));
             final WearSearchData.Track[] tracks = results.tracks;
             if (tracks != null) {
                 for (int i = 0; i < tracks.length; i++) {
@@ -80,9 +82,6 @@ final class SearchResultsAdapter
     @Override
     public int getItemViewType(final int position) {
         final SearchResultsAdapterItem item = getItem(position);
-        if (SearchResultsAdapterItemHeader.class.equals(item.getClass())) {
-            return VIEW_TYPE_HEADER;
-        }
         if (SearchResultsAdapterItemAlbum.class.equals(item.getClass())) {
             return VIEW_TYPE_ALBUM;
         }
@@ -97,19 +96,37 @@ final class SearchResultsAdapter
 
     @Override
     public SearchResultsViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-        switch (viewType) {
-            case VIEW_TYPE_HEADER:
-                return new SearchResultsViewHolder(getLayoutInflater()
-                        .inflate(R.layout.list_item_search_results_header, parent, false));
-
-            default:
-                return new SearchResultsViewHolder(getLayoutInflater()
-                        .inflate(R.layout.list_item_search_results_result, parent, false));
-        }
+        return new SearchResultsViewHolder(getLayoutInflater()
+                .inflate(R.layout.list_item_search_result, parent, false));
     }
 
     @Override
     public void onBindViewHolder(final SearchResultsViewHolder holder, final int position) {
         holder.textView.setText(getItem(position).getTitle());
+        final Drawable drawableStart = drawableStart(getItemViewType(position));
+        if (holder.textView.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            holder.textView.setCompoundDrawablesWithIntrinsicBounds(
+                    null, null, drawableStart, null);
+        } else {
+            holder.textView.setCompoundDrawablesWithIntrinsicBounds(
+                    drawableStart, null, null, null);
+        }
+    }
+
+    @Nullable
+    private Drawable drawableStart(final int viewType) {
+        switch (viewType) {
+            case VIEW_TYPE_ALBUM:
+                return mIconAlbum;
+
+            case VIEW_TYPE_ARTIST:
+                return mIconArtist;
+
+            case VIEW_TYPE_TRACK:
+                return mIconTrack;
+
+            default:
+                return null;
+        }
     }
 }
