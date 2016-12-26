ebfd81106299c6cc77ef0c8d7f0556c38079e23e
Yaroslav Mytkalyk <docd1990@gmail.com>
2016-12-26 15:05:29 +0200

    Added LICENSE.

--- app/src/androidTest/java/com/doctoror/fuckoffmusicplayer/util/SqlUtilsAndroidTest.java
+++ app/src/androidTest/java/com/doctoror/fuckoffmusicplayer/util/SqlUtilsAndroidTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 Yaroslav Mytkalyk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.doctoror.fuckoffmusicplayer.util;
 
 import org.junit.Test;
@@ -8,9 +23,8 @@ import android.support.test.runner.AndroidJUnit4;
 import static org.junit.Assert.*;
 
 /**
- * Created by Yaroslav Mytkalyk on 26.12.16.
+ * {@link SqlUtils} android test
  */
-
 @RunWith(AndroidJUnit4.class)
 public final class SqlUtilsAndroidTest {
 
