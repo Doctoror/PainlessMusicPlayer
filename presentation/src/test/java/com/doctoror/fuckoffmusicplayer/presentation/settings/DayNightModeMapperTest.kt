17cae2094ecfb114fdd0d39bfc8e762d4e076e46
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-03-30 19:21:59 +0300

    Add DayNightModeMapperTest

---
+++ presentation/src/test/java/com/doctoror/fuckoffmusicplayer/presentation/settings/DayNightModeMapperTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018 Yaroslav Mytkalyk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.doctoror.fuckoffmusicplayer.presentation.settings
+
+import android.support.v7.app.AppCompatDelegate
+import com.doctoror.fuckoffmusicplayer.domain.settings.Theme
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class DayNightModeMapperTest {
+
+    private val underTest = DayNightModeMapper()
+
+    @Test
+    fun returnsModeNightNoForThemeDay() {
+        assertEquals(AppCompatDelegate.MODE_NIGHT_NO, underTest.toDayNightMode(Theme.DAY))
+    }
+
+    @Test
+    fun returnsModeNightYesForThemeNight() {
+        assertEquals(AppCompatDelegate.MODE_NIGHT_YES, underTest.toDayNightMode(Theme.NIGHT))
+    }
+
+    @Test
+    fun returnsModeNightAutoNoForThemeDayNight() {
+        assertEquals(AppCompatDelegate.MODE_NIGHT_AUTO, underTest.toDayNightMode(Theme.DAYNIGHT))
+    }
+}
