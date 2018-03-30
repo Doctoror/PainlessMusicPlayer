45bee7b5a53ac1c55963699fa8d1d2f7168f335e
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-03-30 19:18:46 +0300

    Add ThemeToButtonIdMapperTest

--- presentation/src/test/java/com/doctoror/fuckoffmusicplayer/presentation/settings/ThemeToButtonIdMapperTest.kt
+++ presentation/src/test/java/com/doctoror/fuckoffmusicplayer/presentation/settings/ThemeToButtonIdMapperTest.kt
@@ -1,7 +1,66 @@
+/*
+ * Copyright (C) 2018 Yaroslav Mytkalyk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.doctoror.fuckoffmusicplayer.presentation.settings
 
-/**
- * Created by doctor on 30/03/18.
- */
+import com.doctoror.fuckoffmusicplayer.R
+import com.doctoror.fuckoffmusicplayer.domain.settings.Theme
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
 class ThemeToButtonIdMapperTest {
+
+    private val underTest = ThemeToButtonIdMapper()
+
+    @Test
+    fun returnsRadioDayForThemeDay() {
+        assertEquals(R.id.radioDay, underTest.themeToButtonId(Theme.DAY))
+    }
+
+    @Test
+    fun returnsRadioNightForThemeNight() {
+        assertEquals(R.id.radioNight, underTest.themeToButtonId(Theme.NIGHT))
+    }
+
+    @Test
+    fun returnsRadioDayNightForThemeDayNight() {
+        assertEquals(R.id.radioDayNight, underTest.themeToButtonId(Theme.DAYNIGHT))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun throwsIllegalArgumentExceptionForInvalidTheme() {
+        underTest.themeToButtonId(666)
+    }
+
+    @Test
+    fun returnsThemeDayForRadioDay() {
+        assertEquals(Theme.DAY, underTest.buttonIdToTheme(R.id.radioDay))
+    }
+
+    @Test
+    fun returnsThemeNightForRadioNight() {
+        assertEquals(Theme.NIGHT, underTest.buttonIdToTheme(R.id.radioNight))
+    }
+
+    @Test
+    fun returnsThemeDayNightForRadioDayNight() {
+        assertEquals(Theme.DAYNIGHT, underTest.buttonIdToTheme(R.id.radioDayNight))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun throwsIllegalArgumentExceptionForInvalidButtonId() {
+        underTest.buttonIdToTheme(0)
+    }
 }
