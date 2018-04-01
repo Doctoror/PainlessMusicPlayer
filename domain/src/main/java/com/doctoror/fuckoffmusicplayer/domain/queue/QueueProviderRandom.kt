469f2ecb32c6685e89ed828011499fb051a13339
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-04-01 10:20:25 +0300

    Convert QueueProviderRandom to Kotlin

--- domain/src/main/java/com/doctoror/fuckoffmusicplayer/domain/queue/QueueProviderRandom.kt
+++ domain/src/main/java/com/doctoror/fuckoffmusicplayer/domain/queue/QueueProviderRandom.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Yaroslav Mytkalyk
+ * Copyright (C) 2018 Yaroslav Mytkalyk
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,20 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.doctoror.fuckoffmusicplayer.domain.queue;
+package com.doctoror.fuckoffmusicplayer.domain.queue
 
-import android.support.annotation.NonNull;
-
-import java.util.List;
-
-import io.reactivex.Observable;
+import io.reactivex.Observable
 
 /**
- * "Random playlist" provider
+ * "Random queue" provider
  */
-public interface QueueProviderRandom {
+interface QueueProviderRandom {
 
-    @NonNull
-    Observable<List<Media>> randomQueue();
-
+    fun randomQueue(): Observable<List<Media>>
 }
