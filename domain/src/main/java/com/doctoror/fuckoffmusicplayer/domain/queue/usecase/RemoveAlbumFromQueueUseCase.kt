289f687380b54110a77da178d29639315ca19a63
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-04-01 10:21:51 +0300

    Convert RemoveAlbumFromQueueUseCase to Kotlin

--- domain/src/main/java/com/doctoror/fuckoffmusicplayer/domain/queue/usecase/RemoveAlbumFromQueueUseCase.kt
+++ domain/src/main/java/com/doctoror/fuckoffmusicplayer/domain/queue/usecase/RemoveAlbumFromQueueUseCase.kt
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.doctoror.fuckoffmusicplayer.domain.queue.usecase;
+package com.doctoror.fuckoffmusicplayer.domain.queue.usecase
 
-public interface RemoveAlbumFromQueueUseCase {
+interface RemoveAlbumFromQueueUseCase {
 
-    void removeAlbumFromCurrentQueue(long albumId);
+    fun removeAlbumFromCurrentQueue(albumId: Long)
 }
