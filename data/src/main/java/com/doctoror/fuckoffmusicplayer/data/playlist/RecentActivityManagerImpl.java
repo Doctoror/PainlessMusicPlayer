1de98b8e5a6513fa41374a432d47122075187ad4
Yaroslav Mytkalyk <docd1990@gmail.com>
2019-03-08 20:27:33 +0200

    Increase recently played max to 12

--- data/src/main/java/com/doctoror/fuckoffmusicplayer/data/playlist/RecentActivityManagerImpl.java
+++ data/src/main/java/com/doctoror/fuckoffmusicplayer/data/playlist/RecentActivityManagerImpl.java
@@ -37,7 +37,7 @@ import io.reactivex.Completable;
  */
 public final class RecentActivityManagerImpl implements RecentActivityManager {
 
-    private static final int MAX_LENGTH = 10;
+    private static final int MAX_LENGTH = 12;
 
     private static final String FILE_NAME_RECENT_PLAYLISTS = "recent_playlists_albums";
 
